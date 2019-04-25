38e7809a0125a4d9260435eae367d9da96fc56be
Pavel <pasha0593@mail.ru>
2019-04-25 16:15:18 +0300

    25/04/19

--- js/script.js
+++ js/script.js
@@ -9,16 +9,21 @@
      * @constructor
      */
     function Film() {
+        // data provider
         this.filmData = [];
+        this.tagsData = [];
 
+        // options
         this.offset = 0;
         this.pageLimit = 15;
-        this.searchQuery = null;
         this.resCount = 1;
-        this.tagsData = [];
         this.$container = null;
         this.$btnMore = null;
-        this.resTags = [];
+
+        // search values
+        this.searchQuery = null;
+        this.searchTags = [];
+
         var self = this;
 
         this.loadJsonData = function () {
@@ -26,11 +31,6 @@
                 self.filmData = filmDataJson;
                 self.renderList();
             }, 100);
-
-            // $.getJSON('films.json', function (data) {
-            //     self.filmData = data;
-            //     self.renderList();
-            // });
         };
 
         this.loadJsonTags = function () {
@@ -40,10 +40,132 @@
             }, 100);
         };
 
-        // $.getJSON('tags.json', function (data) {
-        //     self.tagsData = data;
-        //     self.renderTags();
-        // });
+        this.getPaginationData = function () {
+            var resData = [],
+                limit = (this.offset + this.pageLimit - 1);
+
+            if (limit > this.filmData.length) {
+                limit = this.filmData.length - 1;
+            }
+
+            if (this.searchTags.length > 0 || this.searchQuery) {
+                for (var item = 1; item <= this.pageLimit + 1;) {
+                    if (this.endFilmList()) {
+                        break;
+                    }
+
+                    var title = this.filmData[this.offset].title.toLowerCase(),
+                        tags = this.filmData[this.offset].tags;
+
+                    if (this._queryFilter({
+                        title: title,
+                        tags: tags
+                    })) {
+                        if (item <= this.pageLimit) {
+                            resData.push(this.filmData[this.offset]);
+                        }
+
+                        item++;
+                    }
+
+                    this.offset++;
+                }
+            } else {
+                for (var i = this.offset; this.offset <= limit; i++) {
+                    resData.push(this.filmData[i]);
+
+                    this.offset++;
+                }
+            }
+
+            if (this.endFilmList()) {
+                this.$btnMore.hide();
+            } else {
+                this.$btnMore.show();
+            }
+
+            return resData;
+        };
+
+        this._queryFilter = function (queryParams) {
+            var res = false;
+
+            if (this.searchQuery) {
+                if (queryParams.title.indexOf(this.searchQuery.toLowerCase()) !== -1) {
+                    res = true;
+                } else {
+                    return false;
+                }
+            }
+
+            if (this.searchTags.length > 0) {
+                if (this._inTags(this.searchTags, queryParams.tags)) {
+                    res = true;
+                } else {
+                    return false;
+                }
+            }
+
+            return res;
+        };
+
+        this._inTags = function (search, tags) {
+            for (var i = 0; i < search.length; i++) {
+                if (tags.indexOf(search[i]) !== -1) {
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        /**
+         * @param value
+         */
+        this.setSearchQuery = function (value) {
+            this.searchQuery = value ? value : null;
+            this.$container.html('');
+            this.offset = 0;
+            this.resCount = 1;
+            this.renderList();
+        };
+
+        this.setTagsQuery = function (tagName) {
+            if (tagName) {
+                this.searchTags.push(tagName);
+                this.$container.html('');
+                this.offset = 0;
+                this.resCount = 1;
+                this.renderList();
+            }
+        };
+
+        /**
+         * @param tagName - selected tag name
+         */
+        this.unsetTagsQuery = function (tagName) {
+            var index = this.searchTags.indexOf(tagName);
+
+            if (index > -1) {
+                this.$container.html('');
+                this.offset = 0;
+                this.resCount = 1;
+                this.renderList();
+
+                this.searchTags.splice(index, 1)
+            }
+        };
+
+        this.renderList = function () {
+            var filmData = this.getPaginationData(),
+                htmlData = '';
+
+            for (var i = 0; i < filmData.length; i++) {
+                htmlData += '<p>#' + this.resCount + ': ' + filmData[i].title + '<a class="js-star">' + '<span title="Добавить в закладки" class="addStarFavorites active">' + '&#9734;' + '</span>' + '<span title="Удалить из закладок" class="removeStarFavorites">' + '&#9733;' + '</span>' + '</a> ' + '</p>';
+                this.resCount++;
+            }
+
+            this.$container.append(htmlData);
+        };
 
         this.renderTags = function () {
             var htmlData = '';
@@ -55,113 +177,16 @@
             this.$containerTags.append(htmlData);
         };
 
-
-        this.getPaginationData = function () {
-            var resData = [],
-                limit = (this.offset + this.pageLimit - 1);
-            if (limit > this.filmData.length) {
-                limit = this.filmData.length - 1;
-            }
-
-            if (this.searchQuery || this.resTags.length > 0) {
-                for (var item = 1; item <= this.pageLimit + 1;) {
-                    if (this.endFilmList()) {
-                        break;
-                    }
-
-                    var title = this.filmData[this.offset].title.toLowerCase(),
-                        tags = this.filmData[this.offset].tags,
-                        showButtonMore = false;
-                    if (title.indexOf(this.searchQuery.toLowerCase()) !== -1 && this.inTags(this.resTags,tags)) {
-                        if (item <= this.pageLimit) {
-                            resData.push(this.filmData[this.offset]);
-                        } else {
-                            showButtonMore = true;
-                        }
-
-                        item++;
-                    }
-
-                    this.offset++;
-                }
-
-                if (showButtonMore) {
-                    this.$btnMore.show();
-                } else {
-                    this.$btnMore.hide();
-                }
-            } else {
-                for (var i = this.offset; this.offset <= limit; i++) {
-                    resData.push(this.filmData[i]);
-
-                    this.offset++;
-                }
-
-                this.$btnMore.show();
-            }
-            return resData;
-        };
-
-        this.inTags = function (search, tags) {
-            for (var i = 0; i < search.length; i++) {
-                if (tags.indexOf(search[i]) !== -1) {
-                    return true;
-                }
-            }
-            return false;
-        };
-
-
-
-
-        this.setSearchQuery = function (v) {
-            this.searchQuery = v ? v : null;
-            this.$container.html('');
-            this.offset = 0;
-            this.resCount = 1;
-            this.renderList();
-        };
-
-        this.setSearchTags = function (b) {
-            this.searchTags = b ? b : null;
-            this.$container.html('');
-            this.offset = 0;
-            this.resCount = 1;
-            this.renderList();
-
-            if (this.searchTags) {
-                this.resTags.push(this.searchTags);
-            }
-        };
-
-        this.unsetTags = function (s) {
-            var index = this.resTags.indexOf(s);
-            this.$container.html('');
-            this.offset = 0;
-            this.resCount = 1;
-            this.renderList();
-
-            if (index > -1) {
-                this.resTags.splice(index, 1)
-            }
-        };
-
-        this.renderList = function () {
-            var filmData = this.getPaginationData(),
-                htmlData = '';
-
-            for (var i = 0; i < filmData.length; i++) {
-                htmlData += '<p>#' + this.resCount + ': ' + filmData[i].title + '</p>';
-                this.resCount++;
-            }
-
-            this.$container.append(htmlData);
-        };
-
+        /**
+         * @returns {boolean}
+         */
         this.endFilmList = function () {
             return this.filmData.length <= this.offset;
         };
 
+        /**
+         * @param opt
+         */
         this.init = function (opt) {
             this.$container = opt.container;
             this.$containerTags = opt.containerTags;
@@ -183,10 +208,6 @@
         e.preventDefault();
 
         film.renderList();
-
-        if (film.endFilmList()) {
-            film.$btnMore.hide();
-        }
     });
 
 
@@ -195,12 +216,31 @@
     });
 
     $(document).on('change', '.js-checkbox', function (e) {
-        if ($(this).is(':checked')) {
-            film.setSearchTags($(this).val());
+        var $this = $(this);
+
+        if ($this.is(':checked')) {
+            film.setTagsQuery($this.val());
         } else {
-            film.unsetTags($(this).val());
+            film.unsetTagsQuery($this.val());
         }
     });
 
+    $(document).on('click', '.js-star', function (e) {
+        e.preventDefault();
+        var $this = $(this);
+        $addStar = $('.addStarFavorites', $this);
+        $removeStar = $('.removeStarFavorites', $this);
+
+        if ($removeStar.hasClass('active')) {
+            $removeStar.removeClass('active');
+        } else {
+            $removeStar.addClass('active');
+        }
+        if ($addStar.hasClass('active')) {
+            $addStar.removeClass('active');
+        } else {
+            $addStar.addClass('active');
+        }
+    });
 })(jQuery);
 
