58ed910f6268abc1f059c9b51ed4cbb20ab32d90
Pavel <pasha0593@mail.ru>
2019-04-26 09:24:02 +0300

    26.04.19

--- js/script.js
+++ js/script.js
@@ -12,12 +12,14 @@
         // data provider
         this.filmData = [];
         this.tagsData = [];
+        this.favoritesFilmData = [];
 
         // options
         this.offset = 0;
         this.pageLimit = 15;
         this.resCount = 1;
         this.$container = null;
+        this.$containerBookmarks = null;
         this.$btnMore = null;
 
         // search values
@@ -146,12 +148,12 @@
             var index = this.searchTags.indexOf(tagName);
 
             if (index > -1) {
+                this.searchTags.splice(index, 1);
+
                 this.$container.html('');
                 this.offset = 0;
                 this.resCount = 1;
                 this.renderList();
-
-                this.searchTags.splice(index, 1)
             }
         };
 
@@ -160,11 +162,30 @@
                 htmlData = '';
 
             for (var i = 0; i < filmData.length; i++) {
-                htmlData += '<p>#' + this.resCount + ': ' + filmData[i].title + '<a class="js-star">' + '<span title="Добавить в закладки" class="addStarFavorites active">' + '&#9734;' + '</span>' + '<span title="Удалить из закладок" class="removeStarFavorites">' + '&#9733;' + '</span>' + '</a> ' + '</p>';
+                htmlData += '<p>#' + this.resCount + ': ' + filmData[i].title + this._getStarTemplate(filmData[i].title, false) +'</p>';
                 this.resCount++;
             }
 
             this.$container.append(htmlData);
+        };
+
+        this._getStarTemplate = function (filmTitle, renderBookmarks) {
+            renderBookmarks = renderBookmarks || false;
+
+            var addStarFavoritesActive = '',
+                removeStarFavorites = '';
+
+            if (this.hasFavoritesByName(filmTitle)) {
+                removeStarFavorites = ' active';
+            } else {
+                addStarFavoritesActive = ' active';
+            }
+
+            return '\
+                <a class="js-star" data-film= "' + filmTitle + '" data-render-bookmarks="'+renderBookmarks+'">\
+                    <span title="Добавить в закладки" class="addStarFavorites'+addStarFavoritesActive+'">&#9734;</span>\
+                    <span title="Удалить из закладок" class="removeStarFavorites'+removeStarFavorites+'">&#9733;</span>\
+                </a>';
         };
 
         this.renderTags = function () {
@@ -184,6 +205,34 @@
             return this.filmData.length <= this.offset;
         };
 
+        this.addFavorites =function (filmName) {
+            this.favoritesFilmData.push(filmName);
+        };
+
+        this.removeFavorites =function (filmName) {
+            var index = this.favoritesFilmData.indexOf(filmName);
+
+            if (index > -1) {
+                this.favoritesFilmData.splice(index, 1)
+            }
+        };
+
+        this.hasFavoritesByName =function (filmName) {
+            return this.favoritesFilmData.indexOf(filmName) !== -1;
+        };
+
+        this.renderBookmarksList = function () {
+            this.$containerBookmarks.html('');
+
+            var htmlData = '';
+
+            for (var i = 0; i < this.favoritesFilmData.length; i++) {
+                htmlData += '<p>#' + (i+1) + ': ' + this.favoritesFilmData[i]+ this._getStarTemplate(this.favoritesFilmData[i], true)+'</p>';
+            }
+
+            this.$containerBookmarks.html(htmlData);
+        };
+
         /**
          * @param opt
          */
@@ -191,6 +240,7 @@
             this.$container = opt.container;
             this.$containerTags = opt.containerTags;
             this.$btnMore = opt.btnMore;
+            this.$containerBookmarks = opt.containerBookmarks;
             this.loadJsonData();
             this.loadJsonTags();
         }
@@ -200,6 +250,7 @@
 
     film.init({
         container: $('.js-films'),
+        containerBookmarks: $('.js-films-bookmarks'),
         btnMore: $('.js-load-more-btn'),
         containerTags: $('.js-tags')
     });
@@ -215,6 +266,17 @@
         film.setSearchQuery($(this).val());
     });
 
+    $('.js-bookmarks-tab').on('click', function (e) {
+        film.renderBookmarksList();
+    });
+
+    $('.js-films-tab').on('click', function (e) {
+        film.$container.html('');
+        film.offset = 0;
+        film.resCount = 1;
+        film.renderList();
+    });
+
     $(document).on('change', '.js-checkbox', function (e) {
         var $this = $(this);
 
@@ -227,19 +289,27 @@
 
     $(document).on('click', '.js-star', function (e) {
         e.preventDefault();
-        var $this = $(this);
-        $addStar = $('.addStarFavorites', $this);
-        $removeStar = $('.removeStarFavorites', $this);
+        var $this = $(this),
+            $addStar = $('.addStarFavorites', $this),
+            $removeStar = $('.removeStarFavorites', $this);
 
-        if ($removeStar.hasClass('active')) {
-            $removeStar.removeClass('active');
+        if (!film.hasFavoritesByName($this.data('film'))) {
+            film.addFavorites($this.data('film'));
+        } else {
+            film.removeFavorites($this.data('film'));
+        }
+
+        $removeStar.removeClass('active');
+        $addStar.removeClass('active');
+
+        if (!film.hasFavoritesByName($this.data('film'))) {
+            $addStar.addClass('active');
         } else {
             $removeStar.addClass('active');
         }
-        if ($addStar.hasClass('active')) {
-            $addStar.removeClass('active');
-        } else {
-            $addStar.addClass('active');
+
+        if ($this.data('render-bookmarks')) {
+            film.renderBookmarksList();
         }
     });
 })(jQuery);
